ceafa04f1813a7bc7e535050f324e1cf84e7c3eb
Neil Rutledge <neilrutledgephoto@gmail.com>
2021-12-07 13:31:55 -0500

    Make hashes uniform with mock data from API

--- src/mocks.ts
+++ src/mocks.ts
@@ -1,11 +1,11 @@
-import { InformationNft, NftId, Rational, UserId } from './common';
+import { Rational, UserId } from './common';
 
 const getRandomElement = <T>(list: T[]) =>
   list[Math.floor(Math.random() * list.length)];
 
-export const mockNftId: NftId = {
-  contentHash: 'abcd1234',
-};
+const generateIds = (count: number) => [...Array(count).keys()];
+
+const getRandomPrice = () => BigInt(Math.ceil(Math.random() * 1000000000));
 
 export const mockUserId: UserId = {
   pubKeyHash: 'wxyz7890',
@@ -14,26 +14,26 @@ export const mockUserId: UserId = {
 export const mockTransactionCBORHex =
   '84a500800d80018182581d60a2c20c77887ace1cd986193e4e75babd8993cfd56995cd5cfce609c21a0098968002000e80a0f5f6';
 
+export const makeMockHash = (baseHash: string, suffixNum: number) => {
+  const suffix = suffixNum.toString();
+  return baseHash.substring(0, baseHash.length - suffix.length) + suffix;
+};
+
+export const makeMockNftId = (suffixNum: number) => ({
+  contentHash: makeMockHash('aa00000000', suffixNum),
+});
+
 export const makeMockInformationNfts = (count: number) => {
-  const nfts: InformationNft[] = [];
-
-  const baseHash = 'aa00000000';
-
-  const pubKeyHashes = [
-    'aaaaaaaa',
-    'bbbbbbbb',
-    'cccccccc',
-    'dddddddd',
-    'eeeeeeee',
-  ];
-
-  const getRandomPrice = () => BigInt(Math.ceil(Math.random() * 1000000000));
+  const baseArtistHash = 'ff00000000';
+  const artistHashes = [...Array(22).keys()].map((idNum) =>
+    makeMockHash(baseArtistHash, idNum)
+  );
 
   const heighestBids = [
     undefined,
     {
       bid: BigInt(Math.floor(Math.random() * 100000000)),
-      bidder: { pubKeyHash: getRandomElement(pubKeyHashes) },
+      bidder: { pubKeyHash: getRandomElement(artistHashes) },
     },
   ];
 
@@ -50,20 +50,16 @@ export const makeMockInformationNfts = (count: number) => {
     };
   };
 
-  return [...Array(count).keys()].map((idNum) => {
-    const suffix = idNum.toString();
+  return generateIds(count).map((idNum) => {
     const num1 = Math.floor(Math.random() * 100);
     const num2 = Math.floor(Math.random() * 100);
     const nums: Rational = [num1, num2];
 
     return {
-      id: {
-        contentHash:
-          baseHash.substring(0, baseHash.length - suffix.length) + suffix,
-      },
+      id: makeMockNftId(idNum),
       share: nums.sort(),
-      author: { pubKeyHash: getRandomElement(pubKeyHashes) },
-      owner: { pubKeyHash: getRandomElement(pubKeyHashes) },
+      author: { pubKeyHash: getRandomElement(artistHashes) },
+      owner: { pubKeyHash: getRandomElement(artistHashes) },
       price: getRandomPrice(),
       auctionState: getRandomAuctionState(),
     };
